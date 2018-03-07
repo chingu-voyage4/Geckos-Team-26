c4e1175de6bc67b319a36ce028637517143f53e7
Maira Martins K <mairamartinsk@gmail.com>
2018-03-07 17:52:48 -0300

    Create login form component

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -2,10 +2,58 @@
 import React, { Component } from 'react';
 import './login.css';
 
-const LogIn = () => (
-  <div className="login">
-    <h1>This is the log in page</h1>
- </div>
-)
+class LogIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="ui form">
+        <div className="field">
+          <label>
+            Email
+            <input
+              name="email"
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+        </div>
+        <br />
+        <div className="field">
+          <label>
+            Password
+            <input
+              name="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+        </div>
+        <button className="ui button" type="submit">
+          Log In
+        </button>
+      </form>
+    );
+  }
+}
 
 export default LogIn;
