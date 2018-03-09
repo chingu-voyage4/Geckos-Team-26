59b82772df81f676718fef9aeca9f0ede3c9c5e5
Maira Martins K <mairamartinsk@gmail.com>
2018-03-09 10:33:13 -0300

    Style Footer component

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -3,12 +3,19 @@ import React from "react";
 import "./footer.css";
 
 const Footer = () => (
-  <footer className="ui inverted vertical center aligned segment">
-    <p>
-      Copyright &copy; 2018 Geckos-Team-26 |{" "}
-      <a href="https://github.com/chingu-voyage4/Geckos-Team-26">GitHub</a> |{" "}
-      <a href="https://chingu.io/">Chingu</a>
-    </p>
+  <footer className="ui inverted vertical center aligned segment footer custom-footer">
+    <p>Copyright &copy; 2018 Geckos-Team-26</p>
+    <div className="ui inverted link list">
+      <a
+        className="item"
+        href="https://github.com/chingu-voyage4/Geckos-Team-26"
+      >
+        GitHub
+      </a>
+      <a className="item" href="https://chingu.io/">
+        Chingu
+      </a>
+    </div>
   </footer>
 );
 
