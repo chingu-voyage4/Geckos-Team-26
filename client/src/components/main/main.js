c745f818781ba3364230fec3ad1bf6990a3c04e2
Maira Martins K <mairamartinsk@gmail.com>
2018-03-09 13:03:02 -0300

    Create grid layout for main page features

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -3,41 +3,6 @@ import "./main.css";
 
 const Main = () => (
   <main className="main">
-    <article className="feature-1 card">
-      <h2>Feature 1</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum eius
-        laudantium excepturi.
-      </p>
-    </article>
-    <article className="feature-2">
-      <h2>Feature 2</h2>
-      <p>
-        Consectetur adipisicing elit. Rerum eius laudantium excepturi, lorem
-        ipsum dolor sit amet.
-      </p>
-    </article>
-    <article className="feature-3">
-      <h2>Feature 3</h2>
-      <p>
-        Rerum eius laudantium excepturi. Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit.
-      </p>
-    </article>
-    <article className="feature-4">
-      <h2>Feature 4</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum eius
-        laudantium excepturi.
-      </p>
-    </article>
-    <article className="feature-5">
-      <h2>Feature 5</h2>
-      <p>
-        Consectetur adipisicing elit. Rerum eius laudantium excepturi, lorem
-        ipsum dolor sit amet.
-      </p>
-    </article>
     <div className="ui inverted masthead segment center aligned custom-background-image">
       <div className="custom-display-header">
         <h1 className="ui inverted header custom-logo-font">MyPet</h1>
@@ -46,6 +11,43 @@ const Main = () => (
           Get Started
         </button>
       </div>
+    </div>
+    <div className="custom-grid">
+      <article className="custom-grid_item feature-1">
+        <h2 className="ui header">Feature 1</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum eius
+          laudantium excepturi.
+        </p>
+      </article>
+      <article className="custom-grid_item feature-2">
+        <h2 className="ui header">Feature 2</h2>
+        <p>
+          Consectetur adipisicing elit. Rerum eius laudantium excepturi, lorem
+          ipsum dolor sit amet.
+        </p>
+      </article>
+      <article className="custom-grid_item feature-3">
+        <h2 className="ui header">Feature 3</h2>
+        <p>
+          Rerum eius laudantium excepturi. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit.
+        </p>
+      </article>
+      <article className="custom-grid_item feature-4">
+        <h2 className="ui header">Feature 4</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum eius
+          laudantium excepturi.
+        </p>
+      </article>
+      <article className="custom-grid_item feature-5">
+        <h2 className="ui header">Feature 5</h2>
+        <p>
+          Consectetur adipisicing elit. Rerum eius laudantium excepturi, lorem
+          ipsum dolor sit amet.
+        </p>
+      </article>
     </div>
   </main>
 );
