5e9451e0924d75ec222cd943265c2c9f4dc294fa
Maira Martins K <mairamartinsk@gmail.com>
2018-03-09 11:08:16 -0300

    Style login form

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -21,36 +21,47 @@ class LogIn extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} className="ui form">
-        <div className="field">
-          <label>
-            Email
-            <input
-              name="email"
-              type="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-              required
-            />
-          </label>
+      <div className="ui middle aligned center aligned grid custom-display-form">
+        <div className="column">
+          <h2 className="ui black header">Log In</h2>
+          <form onSubmit={this.handleSubmit} className="ui large form">
+            <div className="ui stacked segment">
+              <div className="field">
+                <div className="ui left icon input">
+                  <i className="user icon" />
+                  <input
+                    name="email"
+                    type="text"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                    required
+                    placeholder="E-mail address"
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <div className="ui left icon input">
+                  <i className="lock icon" />
+                  <input
+                    name="password"
+                    type="password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    required
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+              <button
+                className="ui fluid large yellow submit button"
+                type="submit"
+              >
+                Log In
+              </button>
+            </div>
+          </form>
         </div>
-        <br />
-        <div className="field">
-          <label>
-            Password
-            <input
-              name="password"
-              type="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-              required
-            />
-          </label>
-        </div>
-        <button className="ui button" type="submit">
-          Log In
-        </button>
-      </form>
+      </div>
     );
   }
 }
