e9d15b62907e841be21eacfd37b75bf1cbd92ba7
Craig Sweaton <csweaton@gmail.com>
2018-03-10 14:00:33 +0000

    Refactored auth routes

---
+++ utils/mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const UserModel = require("../models/User");
+const { dbUrl } = require("../utils/config");
+
+const SaveUserToDB = user => {
+  mongoose.connect(dbUrl);
+
+  const newUser = new UserModel({
+    email: user.email,
+    password: user.password
+  });
+
+  return new Promise((resolve, reject) => {
+    newUser
+      .save()
+      .then(() => {
+        mongoose.disconnect();
+        return resolve({ message: "OK" });
+      })
+      .catch(() => {
+        mongoose.disconnect();
+        return reject(new Error("Could not create new user"));
+      });
+  });
+};
+
+const GetUserFromDB = user => {
+  mongoose.connect(dbUrl);
+
+  return new Promise((resolve, reject) => {
+    UserModel.findOne({ email: user.email })
+      .then(doc => {
+        if (doc.length < 1) {
+          return reject(new Error("Could not find user"));
+        }
+        mongoose.disconnect();
+        return resolve(doc);
+      })
+      .catch(() => {
+        mongoose.disconnect();
+        return reject(new Error("Could not find user"));
+      });
+  });
+};
+
+module.exports = {
+  SaveUserToDB,
+  GetUserFromDB
+};
