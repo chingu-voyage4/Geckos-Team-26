b572e06aa2fa0d0f02d2cad16e599cbb254b457c
Garrus Napp <garrusnapp@gmail.com>
2018-03-11 20:50:51 +0100

    fixed errors from linter

--- client/src/components/userform/formmenu.js
+++ client/src/components/userform/formmenu.js
@@ -1,24 +1,22 @@
 import React from "react";
 
-const FormMenu = props => {
-  return (
-    <div className="ui tabular menu">
-      <a
-        onClick={props.toggler}
-        className={props.activeItem === "signup" ? "item active" : "item"}
-        role="button"
-      >
-        Signup
-      </a>
-      <a
-        onClick={props.toggler}
-        className={props.activeItem === "login" ? "item active" : "item"}
-        role="button"
-      >
-        Login
-      </a>
-    </div>
-  );
-};
+const FormMenu = props => (
+  <div className="ui tabular menu">
+    <a
+      onClick={props.toggler}
+      className={props.activeItem === "signup" ? "item active" : "item"}
+      role="button"
+    >
+      Signup
+    </a>
+    <a
+      onClick={props.toggler}
+      className={props.activeItem === "login" ? "item active" : "item"}
+      role="button"
+    >
+      Login
+    </a>
+  </div>
+);
 
 export default FormMenu;
