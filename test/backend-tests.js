b70e9d450950d4fb029a22d09af8dfd053872e4c
Craig Sweaton <csweaton@gmail.com>
2018-03-12 17:12:21 +0000

    Modified hashing tests

--- test/backend-tests.js
+++ test/backend-tests.js
@@ -1,11 +1,12 @@
-const { before, beforeEach } = require("mocha");
+const { before } = require("mocha");
 const { assert } = require("chai");
+
 const { hashPassword, compareHashes } = require("../utils/hashing");
 
 describe("hashPassword", () => {
   let result;
 
-  beforeEach(done => {
+  before(done => {
     hashPassword("Hello World").then(hash => {
       result = hash;
       done();
@@ -15,54 +16,48 @@ describe("hashPassword", () => {
   it("Should return a string", () => {
     assert.isString(result);
   });
-  it("Should have a length of at least 60 characters", () => {
+  it("Should be at least 60 characters long", () => {
     assert.isAtLeast(result.length, 60);
   });
 });
 
-describe("compareHashes", () => {
-  let hashedResult;
-  let compareResult;
+describe("comapareHashes", () => {
+  let result;
 
-  beforeEach(done => {
-    hashPassword("Hello World").then(hash => {
-      hashedResult = hash;
-
-      compareHashes("Hello World", hashedResult).then(isValid => {
-        compareResult = isValid;
-        done();
+  describe("Truthy results", () => {
+    before(done => {
+      hashPassword("Hello World").then(hash => {
+        compareHashes("Hello World", hash).then(value => {
+          result = value;
+          done();
+        });
       });
+    });
+
+    it("Should return a boolean", () => {
+      assert.isBoolean(result);
+    });
+    it("Should return true", () => {
+      assert.isTrue(result);
     });
   });
 
-  it("Should return a boolean", () => {
-    assert.isBoolean(compareResult);
-  });
-  it("Should be true", () => {
-    assert.isTrue(compareResult);
-  });
-});
-
-describe("compareHashes (Falsy)", () => {
-  let hashedResult;
-  let compareResult;
-
-  beforeEach(done => {
-    hashPassword("Hello World").then(hash => {
-      hashedResult = hash;
-
-      compareHashes("this is a false hash", hashedResult).then(isValid => {
-        compareResult = isValid;
-        done();
+  describe("Falsy results", () => {
+    before(done => {
+      hashPassword("Hello World").then(hash => {
+        compareHashes("this is a fake password", hash).then(value => {
+          result = value;
+          done();
+        });
       });
     });
-  });
 
-  it("Should return a boolean", () => {
-    assert.isBoolean(compareResult);
-  });
-  it("Should be false", () => {
-    assert.isFalse(compareResult);
+    it("Should return a boolean", () => {
+      assert.isBoolean(result);
+    });
+    it("Should return false", () => {
+      assert.isFalse(result);
+    });
   });
 });
 
