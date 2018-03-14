764e45355051973c00faf02ca0afe3a53797dbf4
toni@tonigibbons.co.uk <toni@tonigibbons.co.uk>
2018-03-14 23:10:34 +0000

    Add skeleton dashboard component

---
+++ client/src/components/dashboard/dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./dashboard.css";
+
+const Dashboard = () => (
+  <section className="dashboard">
+    <h1>The user dashboard will go here</h1>
+  </section>
+);
+
+export default Dashboard;
