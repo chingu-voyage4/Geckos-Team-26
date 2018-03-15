406ba4ffc40cf31e64da6c44797d9e90be6414b6
Craig Sweaton <csweaton@gmail.com>
2018-03-15 23:52:38 +0000

    Added checking google access token to oauthRoute.js

--- routes/oauthRoute.js
+++ routes/oauthRoute.js
@@ -5,6 +5,7 @@ const randomString = require("randomstring");
 const { hashPassword } = require("../utils/hashing");
 const { GetUserFromDB, SaveUserToDB } = require("../utils/mongo");
 const { jwtSecret } = require("../utils/config");
+const { fetchUrl } = require("fetch");
 
 router.post("/google", (req, res) => {
   const data = {
@@ -16,29 +17,34 @@ router.post("/google", (req, res) => {
     }
   };
 
-  // Check access token
+  fetchUrl(
+    `https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=${
+      data.accessToken
+    }`,
+    (error, meta, body) => {
+      if (JSON.parse(body.toString()).exp < Date.now()) {
+        GetUserFromDB(data.user)
+          .then(() => {
+            const token = jwt.sign(data.user.email, jwtSecret);
+            return res.json({ token });
+          })
+          .catch(() => {
+            hashPassword(randomString.generate()).then(hash => {
+              data.user.password = hash;
 
-  if (true) {
-    GetUserFromDB(data.user)
-      .then(() => {
-        const token = jwt.sign(data.user.email, jwtSecret);
-        return res.json({ token });
-      })
-      .catch(() => {
-        hashPassword(randomString.generate()).then(hash => {
-          data.user.password = hash;
-
-          SaveUserToDB(data.user)
-            .then(() => {
-              const token = jwt.sign(data.user.email, jwtSecret);
-              return res.json({ created: true, token });
-            })
-            .catch(() => res.json({ message: "User not created in db" }));
-        });
-      });
-  } else {
-    res.status(400).json({ message: "Not Authorised" });
-  }
+              SaveUserToDB(data.user)
+                .then(() => {
+                  const token = jwt.sign(data.user.email, jwtSecret);
+                  return res.json({ created: true, token });
+                })
+                .catch(() => res.json({ message: "User not created in db" }));
+            });
+          });
+      } else {
+        res.status(400).json({ message: "Not Authorised" });
+      }
+    }
+  );
 });
 
 module.exports = router;
