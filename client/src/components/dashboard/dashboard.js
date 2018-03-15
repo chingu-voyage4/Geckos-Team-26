a6438ea1233a7c7da6225581123c32dc87977c03
toni@tonigibbons.co.uk <toni@tonigibbons.co.uk>
2018-03-15 12:23:10 +0000

    Add Mock-up of Dashboard and Route to PetForm

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./dashboard.css";
+
+const username = "Toni";
+const pets = ["Morgan", "Cookie"];
+const petList = pets.map(pets => <li>{pets}</li>);
 
 const Dashboard = () => (
   <section className="dashboard">
-    <h1>The user dashboard will go here</h1>
+    <h2>Welcome {username}</h2>
+    <h3>Your Pets</h3>
+    <ul>{petList}</ul>
+    <Link to="/petform" className="addPet">
+      add a pet
+    </Link>
   </section>
 );
 
