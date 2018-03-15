ad0217820fe7583ee2f4efb80f19a8998cabd6bb
toni@tonigibbons.co.uk <toni@tonigibbons.co.uk>
2018-03-15 19:26:56 +0000

    Add owner to Pet schema, ObjectId, ref User

--- models/Pet.js
+++ models/Pet.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const PetSchema = mongoose.Schema;
 
 const pet = new PetSchema({
-  petname: { type: String, required: true },
+  petName: { type: String, required: true },
+  owner: { type: Schema.Objectid, ref: "User", required: true },
   petAvatar: { type: String },
   species: { type: String },
   breed: { type: String },
