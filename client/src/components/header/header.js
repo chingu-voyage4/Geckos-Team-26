b332d5022e6a8bff12405976af312e294239ef51
Maira Martins K <mairamartinsk@gmail.com>
2018-03-16 14:05:28 +0100

    Style header to use stackable responsive menu

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -4,40 +4,24 @@ import { Link, NavLink } from "react-router-dom";
 import "./header.css";
 
 const Header = () => (
-  <header className="ui inverted menu segment">
-    <div className="ui container">
-      <div className="header item yellow">
-        <Link to="/" className="custom-yellow-header">
-          <i className="paw icon custom-hide" />
-          MyPet
-        </Link>
-      </div>
-      <nav className="right item">
-        <div className="ui secondary inverted pointing menu">
-          <NavLink
-            to="/login"
-            activeClassName="is-active active"
-            className="item"
-          >
-            Log in
-          </NavLink>
-          <NavLink
-            to="/signup"
-            activeClassName="is-active active"
-            className="item"
-          >
-            Sign up
-          </NavLink>
-          <NavLink
-            to="/logout"
-            activeClassName="is-active active"
-            className="item"
-          >
-            Log out
-          </NavLink>
-        </div>
-      </nav>
+  <header className="ui inverted menu stackable">
+    <div className="header item yellow">
+      <Link to="/" className="custom-yellow-header">
+        <i className="paw icon custom-hide" />
+        MyPet
+      </Link>
     </div>
+    <nav className="right menu">
+      <NavLink to="/login" activeClassName="is-active active" className="item">
+        Log in
+      </NavLink>
+      <NavLink to="/signup" activeClassName="is-active active" className="item">
+        Sign up
+      </NavLink>
+      <NavLink to="/logout" activeClassName="is-active active" className="item">
+        Log out
+      </NavLink>
+    </nav>
   </header>
 );
 
