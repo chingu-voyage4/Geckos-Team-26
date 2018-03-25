0c53a5081a6331457275c17e8b3c94eaf20c9b0f
toni@tonigibbons.co.uk <toni@tonigibbons.co.uk>
2018-03-25 14:58:39 +0100

    Remove unused constructor from logout component

--- client/src/components/logout/logout.js
+++ client/src/components/logout/logout.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 
 class Logout extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   componentDidMount() {
     this.props.updateUser({});
   }
