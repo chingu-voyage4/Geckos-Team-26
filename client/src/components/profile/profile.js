fc7a4badfc2f6c1b13b9a6d91d4274adec1d0c30
Maira Martins K <mairamartinsk@gmail.com>
2018-04-02 17:07:40 +0200

    Create basic user profile structure

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -1,10 +1,56 @@
 import React from "react";
+import mockData from "./mockUserData.json";
 import "./profile.css";
 
 const Profile = () => (
-  <section className="profile">
-    <h1>The user profile will go here</h1>
-  </section>
+  <div className="ui container min-height">
+    <div className="ui basic segment right aligned">
+      <img
+        src={mockData.userAvatar}
+        alt="Pet Avatar"
+        className="ui avatar image"
+      />
+      Edit profile
+    </div>
+
+    <h1 className="profile-header">{mockData.user}'s profile</h1>
+    <div className="user-grid">
+      <div className="ui card centered">
+        <div className="image">
+          <img
+            src={mockData.userAvatar}
+            alt="User Avatar"
+            className="user-avatar"
+          />
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <div className="ui middle aligned divided list">
+            <div className="item custom-user-spacing">
+              <div class="header">
+                <i class="user icon" /> Username
+              </div>
+              <div className="content">Toni</div>
+            </div>
+            <div className="item custom-user-spacing">
+              <div class="header">
+                <i class="envelope icon" /> Email
+              </div>
+              <div className="content">morgan@thedog.com</div>
+            </div>
+            <div className="item custom-user-spacing">
+              <div class="header">
+                <i class="key icon" /> Password
+              </div>
+              <div className="content">********</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default Profile;
