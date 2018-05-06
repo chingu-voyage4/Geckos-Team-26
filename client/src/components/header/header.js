ba2f4af71a7c6d30ab8c4d58048325951fa5dfb9
toni@tonigibbons.co.uk <toni@tonigibbons.co.uk>
2018-05-06 13:32:40 +0100

    Fix lint error

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -5,25 +5,23 @@ import PrivateMenu from "./privatemenu";
 
 import "./header.css";
 
-const Header = props => {
-  return (
-    <header className="ui inverted menu stackable custom-header">
-      <div className="header item yellow">
-        <Link to="/" className="custom-yellow-header">
-          <i className="paw icon custom-hide" />
-          MyPet
-        </Link>
-      </div>
-      {props.isLoggedIn ? (
-        <PrivateMenu username={props.username} />
-      ) : (
-        <PublicMenu
-          updateActiveItem={props.updateActiveItem}
-          activeItem={props.activeItem}
-        />
-      )}
-    </header>
-  );
-};
+const Header = props => (
+  <header className="ui inverted menu stackable custom-header">
+    <div className="header item yellow">
+      <Link to="/" className="custom-yellow-header">
+        <i className="paw icon custom-hide" />
+        MyPet
+      </Link>
+    </div>
+    {props.isLoggedIn ? (
+      <PrivateMenu username={props.username} />
+    ) : (
+      <PublicMenu
+        updateActiveItem={props.updateActiveItem}
+        activeItem={props.activeItem}
+      />
+    )}
+  </header>
+);
 
 export default Header;
